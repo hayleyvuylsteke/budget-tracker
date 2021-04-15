7c2e6ca8f1637499268b40e540faf9abb2dfc8b4
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-04-15 12:52:17 -0400

    fixed some service worker issues

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const APP_PREFIX = 'BudgetTrack-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-//const DATA_CACHE_NAME = "data-cache-" + VERSION;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
 const FILES_TO_CACHE = [
   "./index.html",
@@ -19,6 +19,21 @@ const FILES_TO_CACHE = [
   "./icons/icon-512x512.png"
 ];
 
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { 
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -27,3 +42,25 @@ self.addEventListener('install', function (e) {
     })
   )
 })
+
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
